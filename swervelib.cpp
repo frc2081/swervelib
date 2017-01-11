b3deea3383ab990991ee54d7f8b6c7f3577246e3
Lunar Dust <infinitycreeper1@gmail.com>
2017-01-10 18:18:13 -0600

    Re-write

--- swervelib.cpp
+++ swervelib.cpp
@@ -3,76 +3,85 @@
 #include <algorithm>
 #include "swervelib.hpp"
 
-static double degrees_to_radians(double degrees) {
-  double radians;
-  radians = (degrees * M_PI)/180;
-  return radians;
+double degrees_to_radians(double degrees) {
+	double radians;
+	radians = (degrees * PI)/180;
+	return radians;
 }
 
-static double radians_to_degrees(double radians) {
-  double degrees;
-  degrees = radians * 180/M_PI;
-  return degrees;
+double radians_to_degrees(double radians) {
+	double degrees;
+	degrees = radians * 180/PI;
+	return degrees;
 }
 
 swervelib::swervelib(double wheelbase, double trackwidth) {
-  R = sqrt(pow(wheelbase, 2) + pow(trackwidth, 2));
-  _wheelbase = wheelbase;
+	target_WS1 = 0;
+	target_WS2 = 0;
+	target_WS3 = 0;
+	target_WS4 = 0;
+	MAX_WS = 0;
+
+	A = 0;
+	B = 0;
+	C = 0;
+	D = 0;
+	R = sqrt(pow(wheelbase, 2) + pow(trackwidth, 2));
+	_wheelbase = wheelbase;
+
+	whl = new wheel();
 }
 
-swervelib::wheel swervelib::calcWheelVect(
-        double x,
-        double y,
-        double rudder) {
+void swervelib::calcWheelVect(double x,
+						 double y,
+						 double rudder) {
 
-  A = x - rudder * (_wheelbase/R);
-  B = x + rudder * (_wheelbase/R);
-  C = y - rudder * (_wheelbase/R);
-  D = y + rudder * (_wheelbase/R);
+	A = x - rudder * (_wheelbase/R);
+	B = x + rudder * (_wheelbase/R);
+	C = y - rudder * (_wheelbase/R);
+	D = y + rudder * (_wheelbase/R);
 
-  target_WS1 = sqrt(pow(B, 2) + pow(C, 2));
-  target_WS2 = sqrt(pow(B, 2) + pow(D, 2));
-  target_WS3 = sqrt(pow(A, 2) + pow(D, 2));
-  target_WS4 = sqrt(pow(A, 2) + pow(C, 2));
-  MAX_WS = std::max({target_WS1, target_WS2, target_WS3, target_WS4});
-  wheel *whl = new swervelib::wheel;
+	target_WS1 = sqrt(pow(B, 2) + pow(C, 2));
+	target_WS2 = sqrt(pow(B, 2) + pow(D, 2));
+	target_WS3 = sqrt(pow(A, 2) + pow(D, 2));
+	target_WS4 = sqrt(pow(A, 2) + pow(C, 2));
+	MAX_WS = std::max({target_WS1, target_WS2, target_WS3, target_WS4});
 
-  // This is a mess
-  if (target_WS1 > 1) {
-    whl->speed1 = target_WS1/MAX_WS;
-  } else {
-    whl->speed1 = target_WS1;
-  }
-  if (target_WS2 > 1) {
-    whl->speed2 = target_WS2/MAX_WS;
-  } else {
-    whl->speed2 = target_WS2;
-  }
-  if (target_WS3 > 1) {
-    whl->speed3 = target_WS3/MAX_WS;
-  } else {
-    whl->speed3 = target_WS3;
-  }
-  if (target_WS4 > 1) {
-    whl->speed4 = target_WS4/MAX_WS;
-  } else {
-    whl->speed4 = target_WS4;
-  }
+	// This is a mess
+	if (target_WS1 > 1) {
+		this->whl->speed1 = target_WS1/MAX_WS;
+	} else {
+		this->whl->speed1 = target_WS1;
+	}
+	if (target_WS2 > 1) {
+		this->whl->speed2 = target_WS2/MAX_WS;
+	} else {
+		this->whl->speed2 = target_WS2;
+	}
+	if (target_WS3 > 1) {
+		this->whl->speed3 = target_WS3/MAX_WS;
+	} else {
+		this->whl->speed3 = target_WS3;
+	}
+	if (target_WS4 > 1) {
+		this->whl->speed4 = target_WS4/MAX_WS;
+	} else {
+		this->whl->speed4 = target_WS4;
+	}
 
-  /*
-  * Gives the angle in radians between the positive X axis and the point given
-  * by the coordinates.  The result is then converted to degrees.
-  */
-  if (x > 0 && y > 0 && rudder > 0) {
-    whl->angle1 = radians_to_degrees(atan2(B, C));
-    whl->angle2 = radians_to_degrees(atan2(B, D));
-    whl->angle3 = radians_to_degrees(atan2(A, D));
-    whl->angle4 = radians_to_degrees(atan2(A, C));
-  } else {
-    whl->angle1 = 0;
-    whl->angle2 = 0;
-    whl->angle3 = 0;
-    whl->angle4 = 0;
-  }
-  return swervelib::wheel();
+	/*
+	* Gives the angle in radians between the positive X axis and the point given
+	* by the coordinates.  The result is then converted to degrees.
+	*/
+	if (x > 0 && y > 0 && rudder > 0) {
+		this->whl->angle1 = radians_to_degrees(atan2(B, C));
+		this->whl->angle2 = radians_to_degrees(atan2(B, D));
+		this->whl->angle3 = radians_to_degrees(atan2(A, D));
+		this->whl->angle4 = radians_to_degrees(atan2(A, C));
+	} else {
+		this->whl->angle1 = 0;
+		this->whl->angle2 = 0;
+		this->whl->angle3 = 0;
+		this->whl->angle4 = 0;
+	}
 }
--- swervelib.h
+++ swervelib.h
@@ -5,19 +5,28 @@
 
 #include <cmath>
 
-static double degrees_to_radians(double degrees);
-static double radians_to_degrees(double radians);
-class swervelib {
+const double PI = 3.14159265;
+
+class wheel {
 public:
-    double A, B, C, D, R, _wheelbase;
-    double target_WS1, target_WS2, target_WS3, target_WS4, MAX_WS;
+	// front right, front left, rear left, rear right
+	double speed1, speed2, speed3, speed4;
+	// front right, front left, rear left, rear right
+	double angle1, angle2, angle3, angle4;
+};
+
+class swervelib {
+private:
+	double target_WS1, target_WS2, target_WS3, target_WS4, MAX_WS;
+	double A, B, C, D, R, _wheelbase;
+
+public:
     swervelib(double wheelbase, double trackwidth);
-    static struct wheel {
-        // front right, front left, rear left, rear right
-        double speed1, speed2, speed3, speed4;
-        // front right, front left, rear left, rear right
-        double angle1, angle2, angle3, angle4;
-    } whl;
-    wheel calcWheelVect(double x, double y, double rudder);
+
+    virtual ~swervelib() = default;
+
+    wheel *whl;
+
+    void calcWheelVect(double x, double y, double rudder);
 };
 #endif //SWERVE_TEST_SWERVELIB_HPP_H
